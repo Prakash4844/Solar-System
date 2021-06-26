ec7a16a24780f57d41a26ac6354bb46c637ccf2f
Prakash <pk484442@gmail.com>
2021-06-26 21:29:02 +0530

    [Implemented] Planet, Sun Rotation on their axis  and Around each other.
    Sun, Earth and moon Rotating on it's Axis functionality implemented, And Moon and Earth now also Rotate around with respect to earth and Sun respectively

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A class to make projectiles move
+/// </summary>
+public class Projectile : MonoBehaviour
+{
+    
+    [Tooltip("The distance this projectile will move each second in meters.")]
+    public float projectileSpeed = 3.0f;
+
+    [Tooltip("How far away from the main camera before destroying the projectile gameobject in meters.")]
+    public float destroyDistance = 20.0f;
+
+    /// <summary>
+    /// Description:
+    /// Standard Unity function called once per frame
+    /// Inputs: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    private void Update()
+    {
+        MoveProjectile();
+    }
+
+    /// <summary>
+    /// Description:
+    /// Move the projectile in the direction it is heading
+    /// Inputs: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    private void MoveProjectile()
+    {
+        // move the transform
+        transform.position = transform.position + transform.forward * projectileSpeed * Time.deltaTime;
+        
+        // calculate the distance from the main camera
+        float dist = Vector3.Distance(Camera.main.transform.position, transform.position);
+
+        // if the distance is greater than the destroyDistance
+        if (dist>destroyDistance)
+        {
+            Destroy(this.gameObject); // destroy the gameObject
+        }
+    }
+}
